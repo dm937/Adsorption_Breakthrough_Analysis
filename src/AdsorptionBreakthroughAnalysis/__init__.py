833594eac91ccd8cc070f2aa9e8447ea56a5a51c
FMcil <frlmcilwaine@gmail.com>
2023-05-11 14:35:26 +0100

    trial notebook

--- src/AdsorptionBreakthroughAnalysis/__init__.py
+++ src/AdsorptionBreakthroughAnalysis/__init__.py
@@ -5,6 +5,6 @@
 from AdsorptionBreakthroughAnalysis.breakthrough_analysis import ExperimentalSetup  # noqa
 from AdsorptionBreakthroughAnalysis.breakthrough_analysis import experiment_analysis  # noqa
 from AdsorptionBreakthroughAnalysis.breakthrough_analysis import standard_output  # noqa
-
+from AdsorptionBreakthroughAnalysis.version import VERSION as __version__  # noqa
 
 from .api import *  # noqa
